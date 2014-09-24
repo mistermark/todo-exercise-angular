4e48fb04c712d6de21e088c4b3b1325e7dcd0141
Mark de Jong <mark@markdejong.com>
2014-09-24 09:46:47 +0200

    Moved API url to Jekyll config

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
+---
+---
 var app = angular.module("todoConfig", []);
 
 app.factory("settings", function() {
   return {
-    "apiUrl": "http://localhost:5123/api/todos"
+    "apiUrl": "{{ site.api }}"
   };
 });
